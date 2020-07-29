541c09721b242416169bf3eb813a3d80b2990519
Mariana Caramori <musardo@mail.com>
2020-07-29 02:25:29 -0300

    rover moves styled

--- src/components/rover_moves.jsx
+++ src/components/rover_moves.jsx
@@ -6,7 +6,9 @@ class RoverMoves extends Component {
     const { roverId, start, movements } = this.props.rover
     return (
       <div className="rover-item">
-         <b>Rover {roverId + 1}:</b> start[{start.x},{start.y},{start.direction}] - {movements.length > 0 ? movements.map(movement => movement+" ") : "standing still"}
+        <p>
+          <b>Rover {roverId + 1}:</b> start[{start.x},{start.y},{start.direction}] - {movements.length > 0 ? movements.map(movement => movement+" ") : "standing still"}
+        </p>
       </div>
     )
   }
